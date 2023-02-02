1553d90bf619b0ff65cc0f8691efa364aac4aab4
Wesley Scholl <clients@konjoweb.com>
2023-02-02 16:21:58 -0700

    added notification on swipe based on like or dislike, switched swiper

--- App.js
+++ App.js
@@ -1,43 +1,53 @@
-import React, {useRef} from 'react';
-import { Image, StyleSheet, View, TouchableOpacity, Text, Dimensions } from 'react-native';
-
-import CardsSwipe from 'react-native-cards-swipe';
-
-const cardsData = [
-  { src: require('./assets/apple.png') },
-  { src: require('./assets/bananas.png') },
-  { src: require('./assets/cherries.png') },
-  { src: require('./assets/mango.png') },
-  { src: require('./assets/orange.png') },
-  { src: require('./assets/pear.png') },
-  { src: require('./assets/pineapple.png') },
-  { src: require('./assets/strawberry.png') },
-  { src: require('./assets/watermelon.png') },
-  { src: require('./assets/watermelon1.png') },
-];
+import React, {useRef, useState, useEffect} from 'react';
+import { Image, StyleSheet, View, TouchableOpacity, Text, Dimensions, Alert } from 'react-native';
+import Swiper from 'react-native-deck-swiper'
 
 export default function App() {
+  const [images, setImages] = useState([])
   const swipe = useRef(null);
+
+  useEffect(() => {
+    getImages();
+  }, [])
+  
+  function getImages() {
+        fetch("https://picsum.photos/v2/list", {
+          method: "GET"
+        }).then(res => res.json())
+          .then(res => {
+           var imgs = []
+           res.map(img => {imgs.push({src: img.download_url + '?time=' + new Date() })})
+           setImages(imgs)
+          }).catch(error => {
+            console.log(`${error.message}!`);
+          });
+    }
+
   return (
     <View style={styles.container}>
-      <CardsSwipe
+      <Text style={{fontSize: 30, paddingTop: 60, textAlign: 'center', fontWeight: '300'}}>Swiper</Text>
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+      {images.length > 0 && <Swiper
         ref={swipe}
-        cards={cardsData}
-        cardContainerStyle={styles.cardContainer}
+        cards={images}
+        stackSize={10}
+        onSwipedRight={()=>Alert.alert("Liked!")}
+        onSwipedLeft={()=>Alert.alert("Disliked!")}
         renderCard={(card) => (
           <View style={styles.card}>
             <Image
               style={styles.cardImg}
-              source={card.src}
+              source={{uri: card.src}}
             />
           </View>
         )}
-      />
+      />}
+      </View>
       <TouchableOpacity style={[styles.buttons, {backgroundColor: "#9bb783", bottom: 50, right: 50}]} onPress={ () => { swipe.current.swipeRight() }}>
-    <Text style={{color: '#F8F7F1'}}>Right</Text>
+    <Text style={{color: '#F8F7F1'}}>Like</Text>
   </TouchableOpacity>
   <TouchableOpacity style={[styles.buttons, {backgroundColor: "#924D40", bottom: 50, left: 50, }]} onPress={ () => { swipe.current.swipeLeft() }}>
-    <Text style={{color: '#F8F7F1'}}>Left</Text>
+    <Text style={{color: '#F8F7F1'}}>Dislike</Text>
   </TouchableOpacity>
     </View>
   );
@@ -45,17 +55,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:2,
     backgroundColor: '#CAD2D9',
-  },
-  cardContainer: {
-   justifyContent: 'center',
   },
   card: {
     width: Dimensions.get('screen').width * 0.9,
-    height: Dimensions.get('screen').height * 0.6,
+    height: Dimensions.get('screen').height * 0.55,
     backgroundColor: '#F8F7F1',
     borderRadius: 8,
     shadowColor: 'gray',
@@ -69,9 +74,9 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   cardImg: {
-    width: 300,
-    height: 300,
-    borderRadius: 13,
+    width: '100%',
+    height: '100%',
+    borderRadius: 8,
   },
   buttons: {
     shadowColor: 'gray',
